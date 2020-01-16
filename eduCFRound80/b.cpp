30f02d5a885c67abdf8e784f6b3db7a28adcdea3
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-01-16 16:34:28 +0530

    Update b.cpp

--- eduCFRound80/b.cpp
+++ eduCFRound80/b.cpp
@@ -1,9 +1,21 @@
 You are given two integers A and B, calculate the number of pairs (a,b) such that 1≤a≤A, 1≤b≤B, and the equation a⋅b+a+b=conc(a,b) is true; conc(a,b) is the concatenation of a and b (for example, conc(12,23)=1223, conc(100,11)=10011). a and b should not contain leading zeroes.
 
+// daridram entante...codeforces lo pow() pancheyyadu. so power ki oka linear function rayali.
+ 
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
  
+ll pow(ll a)
+{
+	ll res=1;
+	for(int i=1;i<=a;i++)
+	{
+   		res*=10;
+  	}
+  	return res;
+}
+
 int main()
 {
     int t;
@@ -19,7 +31,7 @@ int main()
             d/=10;
             counter++;
         }
-        if(b==(pow(10,counter)-1))
+        if(b==(pow(counter)-1))
         {
             cout<<a*counter<<"\n";
         }
