44e311c802f42ecce172d2e7bdde54814b96217d
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-03-28 13:21:16 +0530

    Add files via upload

---
+++ c1-letters.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+//int pow[N]
+const int N=1e5;
+const int mod=1e9+7;
+
+void solve()
+{
+    int n,m,q;
+    cin>>n>>m;
+
+    int a[n+1],b[n+1];
+    a[0]=0;
+    mem(b,0);
+
+    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<=n;i++) b[i]=a[i]+b[i-1];
+
+    for(int i=0;i<m;i++)
+    {
+        cin>>q;
+        int idx=lower_bound(b+1,b+1+n,q)-b;
+        cout<<idx<<" "<<q-b[idx-1]<<"\n";
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    clock_t t1,t2;
+    t1=clock();
+
+    int t=1;
+    //cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    #ifndef ONLINE_JUDGE
+    t2=clock();
+    cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+    #endif
+
+    return 0;
+}
