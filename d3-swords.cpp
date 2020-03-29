c60d3ff06550878150428e94c10226d22339818d
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-03-29 17:00:11 +0530

    Create d3-swords.cpp

---
+++ d3-swords.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+//int pow[N]
+const int N=1e5;
+const int mod=1e9+7;
+
+void solve()
+{
+    int n,maxEl,gcd,ans=0;
+    cin>>n;
+
+    int a[n];
+    cin>>a[0];
+    maxEl=a[0];
+    vi ar;
+    for(int i=1;i<n;i++) 
+    {
+        cin>>a[i];
+        maxEl=max(maxEl,a[i]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        int dif=maxEl-a[i];
+        if(dif) ar.pb(dif);
+    }
+
+    if(!ar.empty()) gcd=ar[0];
+    else 
+    {
+        cout<<"0 0";
+        return ;
+    }
+
+    for(int i=1;i<ar.size();i++)
+    {
+        gcd=__gcd(gcd,ar[i]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        ans+=(maxEl-a[i])/gcd;
+    }
+
+    cout<<ans<<" "<<gcd ;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    clock_t t1,t2;
+    t1=clock();
+
+    int t=1;
+    //cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    #ifndef ONLINE_JUDGE
+    t2=clock();
+    cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+    #endif
+
+    return 0;
+}
