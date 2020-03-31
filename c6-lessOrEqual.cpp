b66457b6be82005c6b25a04bf54f16980cd9e1d7
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-03-31 16:37:36 +0530

    Add files via upload

---
+++ c6-lessOrEqual.cpp
@@ -0,0 +1,77 @@
+//redCoder laga not thinking...anni conditions should be tested b4 itself....dabba moham
+
+/*
+	YES PAIN YES GAIN
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+//int pow[N]
+const int N=1e5;
+const int mod=1e9+7;
+
+void solve()
+{
+	int n,k;
+	cin>>n>>k;
+
+	int a[n];
+	for(int i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+
+	if(k==0) 
+	{
+		(a[0]==1) ? cout<<"-1" : cout<<a[0]-1 ;
+	}
+	else if(k==n)
+	{
+		cout<<a[n-1];
+	}
+	else (a[k]==a[k-1])  ? cout<<"-1" : cout<<a[k]-1 ;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif
+
+	clock_t t1,t2;
+	t1=clock();
+
+	int t=1;
+	//cin>>t;
+
+	while(t--)
+	{
+		solve();
+	}
+
+	#ifndef ONLINE_JUDGE
+	t2=clock();
+	cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+	#endif
+
+	return 0;
+}
