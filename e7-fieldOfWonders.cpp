ec35ed1b899277a8abe95c1fcc0a94522ecd2aa8
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-06 22:58:36 +0530

    Add files via upload

---
+++ e7-fieldOfWonders.cpp
@@ -0,0 +1,110 @@
+//pindi avatala padesindi.....conditin ardam cheskodam is an art
+//1.a character should appear in all the k(not m...k...i.e valid strings)
+//2.rule invalid strings...ante...already seen character * place lo undadam...and...star leni characters mismatch
+
+/*
+    YES PAIN YES GAIN
+*/
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+const int N=1e5;
+const int M=3e5;
+const int mod=1e9+7;
+
+vi stars;
+unordered_set<char> letters;
+unmap<char,set<string>> strings;
+
+bool valid(string guess,string word)
+{
+    for(int i=0;i<guess.size();i++)
+    {
+        if(word[i]=='*') if(letters.find(guess[i]) != letters.end()) return false;
+        if(word[i]!=guess[i]) return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    int n,m,ans=0;
+    string word,guess;
+
+    cin>>n;
+    cin>>word;
+
+    for(int i=0;i<n;i++) 
+    {
+        if(word[i] != '*') letters.insert(word[i]); 
+        else stars.pb(i);
+    }
+
+    cin>>m;
+    int k=m;
+    for(int i=0;i<m;i++)
+    {
+        cin>>guess;
+        if(!valid(guess,word))
+        {
+            k--;
+            continue;
+        }
+        for(int idx : stars) if(letters.find(guess[idx]) == letters.end()) strings[guess[idx]].insert(guess);
+    }
+
+    for(char c='a';c<='z';c++) if(strings[c].size()==k) ans++;
+
+    cout<<ans;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    clock_t t1,t2;
+    t1=clock();
+
+    int t=1;
+    //cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    #ifndef ONLINE_JUDGE
+    t2=clock();
+    cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+    #endif
+
+    return 0;
+}
