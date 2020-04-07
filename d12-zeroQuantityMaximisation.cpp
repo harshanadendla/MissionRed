952ef96a417ff2f1bc4d342382d3c03b9cb6eb34
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-07 23:01:17 +0530

    Add files via upload

---
+++ d12-zeroQuantityMaximisation.cpp
@@ -0,0 +1,95 @@
+//malli anthe....anni psbl cases poortiga choodakunda...it costed you 3 submissions...ponle idea ventane strike aindi
+
+/*
+    YES PAIN YES GAIN
+*/
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+const int N=1e5;
+const int M=3e5;
+const int mod=1e9+7;
+
+void solve()
+{
+    map<pii,int> d;
+    int gcd,ans=0,zero=0;
+
+    int n;
+    cin>>n;
+
+    int a[n],b;
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>b;
+        if(a[i]==0) 
+        {
+            if(b==0) zero++;
+            continue;
+        }
+
+        gcd=__gcd(abs(a[i]),abs(b));
+
+        b /= gcd;
+        a[i] /= gcd;
+
+        (a[i]<0) ? b=++d[mp(-b,a[i])] : b=++d[mp(b,-a[i])] ;
+
+        if(b>ans) ans=b;
+    }
+
+    cout<<ans+zero;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    clock_t t1,t2;
+    t1=clock();
+
+    int t=1;
+    //cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    #ifndef ONLINE_JUDGE
+    t2=clock();
+    cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+    #endif
+
+    return 0;
+}
