ac4c7ee17116d71b88d3504f1cb295aef98e9d37
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-08 15:42:53 +0530

    Add files via upload

---
+++ e9-MilitaryProblem.cpp
@@ -0,0 +1,119 @@
+//good...idea vachindi...and inko kotta idea kooda mind lo add aindi..number of descendants ki tIn and tOut
+
+/*
+    YES PAIN YES GAIN
+*/
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+
+const int N=2e5+5;
+const int M=3e5;
+const int mod=1e9+7;
+
+int inTime;
+vi ans;
+vi graph[N];
+unmap<int,int> idx;
+bool vis[N];
+int subs[N];
+
+void dfs(int u)
+{
+    subs[u]=inTime;
+
+    vis[u]=true;
+    ans.pb(u);
+    for(int v : graph[u])
+    {
+        if(!vis[v])
+        {
+            inTime++;
+            dfs(v);
+        }
+    }
+
+    subs[u]=inTime-subs[u]+1;
+}
+
+void solve()
+{
+    int n,q,u,k;
+    cin>>n>>q;
+
+    for(int i=2;i<=n;i++)
+    {
+        cin>>u;
+        graph[u].pb(i);
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        sort(all(graph[i]));
+    }
+
+    dfs(1);
+
+    for(int i=0;i<n;i++)
+    {
+        idx[ans[i]]=i;
+    }
+
+    for(int i=1;i<=q;i++)
+    {
+        cin>>u>>k;
+        k--;
+        (k < subs[u]) ? cout<<ans[idx[u]+k] : cout<<"-1" ;
+        cout<<"\n";
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    clock_t t1,t2;
+    t1=clock();
+
+    int t=1;
+    //cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    #ifndef ONLINE_JUDGE
+    t2=clock();
+    cerr<<"timeTaken : "<<(t2-t1)/(double)CLOCKS_PER_SEC;
+    #endif
+
+    return 0;
+}
