074eb2fc05861be680ae6121b72e73ace5dfc0e7
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-21 22:58:29 +0530

    Create A.cpp

---
+++ Div3-636/A.cpp
@@ -0,0 +1,68 @@
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5;
+const int mod=1e9+7;
+
+void solve()
+{
+    int n;
+    cin>>n;
+
+    if(~n == 0)
+    {
+        cout<<"1\n";
+        return;
+    }
+
+    int mask=3;
+    while(1)
+    {
+        if(n%mask==0)
+        {
+            cout<<n/mask<<"\n";
+            return;
+        }
+        mask=((mask<<1)|1);
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
