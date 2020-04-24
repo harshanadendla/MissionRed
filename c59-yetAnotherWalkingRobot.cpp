65ed2aaadd419fa11482c01ca649399477f94a9e
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-24 11:18:33 +0530

    Add files via upload

---
+++ c59-yetAnotherWalkingRobot.cpp
@@ -0,0 +1,78 @@
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5+5;
+const int mod=1e9+7;
+
+void solve() {
+    int n;
+    string str;
+    cin>>n>>str;
+
+    str='.'+str;
+
+    pii p,ans=mp(-1e5,1e5);
+    int x=0,y=0;
+    map<pii,int> m;
+    m[mp(x,y)]=1;
+    for (int i=1;i<=n;i++) {
+        if (str[i]=='L') {
+            x--;
+        } else if (str[i]=='R') {
+            x++;
+        } else if(str[i]=='U') {
+            y++;
+        } else if (str[i]=='D') {
+            y--;
+        }
+        p=mp(x,y);
+        if (m.find(p) != m.end()) {
+            if (ans.s-ans.f > i-m[p]) {
+                ans.f=m[p];
+                ans.s=i;
+            }
+        }
+        m[mp(x,y)]=i+1;
+    }
+
+    (ans.f==-1e5) ? cout<<"-1\n" : cout<<ans.f<<" "<<ans.s<<"\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
