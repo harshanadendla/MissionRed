392876aeff82133cf28e476feb0640a17b81404e
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-25 16:20:42 +0530

    Create C.cpp

---
+++ Div3-605/C.cpp
@@ -0,0 +1,80 @@
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5+5;
+const int mod=1e9+7;
+
+void solve() {
+    int n,k;
+    string str;
+    cin>>n>>k>>str;
+
+    set<char> still;
+
+    for(int i=0;i<k;i++) {
+        char a;
+        cin>>a;
+        still.insert(a);
+    }
+
+    vi idx;
+    idx.pb(-1);
+    for(int i=0;i<n;i++) {
+        if(still.find(str[i]) != still.end()) {
+            continue;
+        }
+        idx.pb(i);
+    }
+    idx.pb(n);
+
+    int ans=0;
+    for(int i=1;i<idx.size();i++) {
+        int l=idx[i]-idx[i-1]-1;
+        ans+=((l*(l+1))/2);
+    }
+
+    cout<<ans;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    //cin>>t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
