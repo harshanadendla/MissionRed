6b1b1d9c2040038f166ff284f511c951cdb38a84
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-04-29 23:54:54 +0530

    Add files via upload

---
+++ c64-boredom.cpp
@@ -0,0 +1,70 @@
+//yes pain...yes gain....took a lot of time...thought in several ways....
+//but finally ardamaindi...idi just maxSum with adj elements dp ani
+
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define dbg(x) cout<<"x "<<x<<"\n"
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5+5;
+const int mod=1e9+7;
+
+void solve() {
+    int n;
+    cin>>n;
+
+    vi cnt(N,0),dp(N,0);
+    for (int i=0,a;i<n;i++) {
+        cin>>a;
+        cnt[a]++;
+    }
+
+    dp[1]=1*cnt[1];
+    for (int i=2;i<N;i++) {
+        dp[i]=max(dp[i-1],dp[i-2]+i*cnt[i]);
+    }
+
+    cout<<dp[N-1];
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    //cin>>t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
