23533c7d70ccd3e5619500e6da7c44479054d6b9
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-05-10 10:17:28 +0530

    Create 5.4-IvanFoolAndProbTheory.cpp

---
+++ DP/5.4-IvanFoolAndProbTheory.cpp
@@ -0,0 +1,78 @@
+//thop asalu idea
+
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define dbg(x) cout<<#x<<" "<<x<<"\n"
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5+5;
+const int mod=1e9+7;
+
+int fib[N];
+
+int Madd(int a,int b) {
+    return ((a%mod)+(b%mod))%mod;
+}
+
+int Mmull(int a,int b) {
+    return ((a%mod)*(b%mod))%mod;
+}
+
+void pre() {
+    fib[0]=1;
+    fib[1]=1;
+    fib[2]=2;
+    for (int i=3;i<N;i++) {
+        fib[i]=Madd(fib[i-1],fib[i-2]);
+    }
+}
+
+void solve() {
+    pre();
+
+    int n,m;
+    cin>>n>>m;
+    cout<<Mmull(2,fib[n]+fib[m]-1)<<'\n';
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    //cin>>t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
