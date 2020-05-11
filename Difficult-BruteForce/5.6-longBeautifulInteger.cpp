2f7d8a89481fe1d976a8c0720149299f9fb9db06
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-05-11 09:09:59 +0530

    Add files via upload

---
+++ Difficult-BruteForce/5.6-longBeautifulInteger.cpp
@@ -0,0 +1,99 @@
+/*
+    YES PAIN YES GAIN
+*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+
+template<size_t>
+struct debug{};
+
+template <class T>
+ostream& print_tuple(ostream& out, const T& t, debug<1> ) {
+    return out << get<tuple_size<T>::value-1>(t);
+}
+
+template <class T, size_t idx>
+ostream& print_tuple(ostream& out, const T& t, debug<idx> ) {
+    out << get< tuple_size<T>::value-idx >(t) << " , ";
+    return print_tuple(out, t, debug<idx-1>());
+}
+
+template <class... Args>
+ostream& operator<<(ostream& out, const tuple<Args...>& t) {
+    out << "\n[";
+    print_tuple(out, t, debug<sizeof...(Args)>());
+    return out << "]\n";
+}
+
+template<class T>
+using rbTree=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define unmap unordered_map
+#define mset multiset
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mem(u,v) memset(u,v,sizeof(u))
+#define all(x) x.begin(),x.end()
+#define r_all(x) x.rbegin(),x.rend()
+#define dbg(x) cerr<<x<<"\n"
+#define bug(x) cout<<"hi"<<x<<endl
+const int N=1e5+5;
+const int mod=1e9+7;
+
+void solve() {
+    int n,k;
+    string x,temp;
+    cin>>n>>k>>x;
+    temp=x;
+    for (int i=k;i<n;i++) {
+        temp[i]=temp[i-k];
+    }
+
+    if (temp >= x) {
+        cout<<n<<'\n'<<temp<<'\n';
+        return;
+    } else {
+        for (int i=k-1;i>=0;i--) {
+            if (temp[i] == '9') {
+                temp[i]='0';
+            } else {
+                temp[i]++;
+                break;
+            }
+        }
+        for (int i=k;i<n;i++) {
+            temp[i]=temp[i-k];
+        }
+    }
+
+    cout<<n<<'\n'<<temp<<'\n';
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t=1;
+    //cin>>t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
